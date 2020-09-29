93624604bd5f2aa0e3d98ee73d1c13d12ba66c6c
DAVID OLASUPO <DAVID.OLASUPO@Sterling.ng>
2020-09-29 21:05:19 +0100

    added sms response

---
+++ FSITest/FSITest/Models/ResponseModel/SmsResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FSITest.Models.ResponseModel
+{
+   
+        public class SmsResponse
+        {
+            public Smsmessagedata SMSMessageData { get; set; }
+        }
+
+        public class Smsmessagedata
+        {
+            public string Message { get; set; }
+            public Recipient[] Recipients { get; set; }
+        }
+
+        public class Recipient
+        {
+            public int statusCode { get; set; }
+            public string number { get; set; }
+            public string cost { get; set; }
+            public string status { get; set; }
+            public string messageId { get; set; }
+        }
+
+}
