070aee40c4f75f01194186f1e22e0127d320f0a5
Azeez <azeez.adedayo@yahoo.com>
2020-09-30 00:49:44 +0100

    Added send airtime method

--- FSITest/FSITest/BLL/ApiCalls.cs
+++ FSITest/FSITest/BLL/ApiCalls.cs
@@ -1,9 +1,15 @@
 ﻿using FSITest.Models;
+using FSITest.Models.RequestModel;
+using FSITest.Models.ResponseModel;
+using Newtonsoft.Json;
 using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace FSITest.BLL
@@ -45,6 +51,50 @@ namespace FSITest.BLL
                 return false;
             }
         }
+
+        public async Task<AirtimeResponseModel> SendAirtme(AirtimeRequestModel requestObject)
+        {
+            AirtimeResponseModel responseModel = new AirtimeResponseModel();
+            string baseUrl = "https://sandboxapi.fsi.ng";
+            string sendAirtime = "/atlabs/airtime/send";
+            string url = baseUrl + sendAirtime;
+
+            string contentType = "application/json";
+
+            using (var client = new HttpClient())
+            {
+                //client.BaseAddress = new Uri("https://sandboxapi.fsi.ng/nibss/bvnr");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Add("Sandbox-Key", "6fee89c336c046c4f0473d004906e3a7");
+
+                client.DefaultRequestHeaders
+                    .Accept
+                    .Add(new MediaTypeWithQualityHeaderValue("application/json"));//ACCEPT header
+
+                client.DefaultRequestHeaders.Add("Accept", contentType);
+
+                //var requestObject = new AirtimeRequestModel();
+
+                //var recipient = new AirtimeRecipient();
+                //var recipients = new List<AirtimeRecipient>();
+                //recipient.amount = "100";
+                //recipient.currencyCode = "NGN";
+                //recipient.phoneNumber = "+2347036135901";
+
+                //requestObject.recipients = new List<AirtimeRecipient>();
+
+                //requestObject.recipients.Add(recipient);
+                var data = new StringContent(JsonConvert.SerializeObject(requestObject));
+                var response = await client.PostAsync(url, data);
+
+                string result = response.Content.ReadAsStringAsync().Result;
+
+                responseModel = JsonConvert.DeserializeObject<AirtimeResponseModel>(result);
+
+                return responseModel;
+            }
+        }
+
     }
 }
 
